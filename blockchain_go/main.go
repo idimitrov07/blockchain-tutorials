17cafcbf4c76114d504290040e0684dd22cb5968
Ivan <idimitrov07@gmail.com>
2018-11-29 00:21:19 +0200

    FInish simple blockchain

--- blockchain_go/main.go
+++ blockchain_go/main.go
@@ -1,4 +1,4 @@
-package main
+package main 
 
 import (
 	"crypto/sha256"
@@ -15,19 +15,18 @@ import (
 	"github.com/joho/godotenv"
 )
 
-// define block
 type Block struct {
-	Index 		int
-	Timestamp string
-	BPM 			int
-	Hash 			string
+	Index 		int 
+	Timestamp string 
+	BPM 			int 
+	Hash 			string 
 	PrevHash 	string
 }
 
-// define blockchain
-var Blockchain []Block
+// define Blockchain as an array of blocks
+var Blockchain []Block 
 
-func calculateHash(block Block) string {
+func calulateHash(block Block) string {
 	record := string(block.Index) + block.Timestamp + string(block.BPM) + block.PrevHash
 	h := sha256.New()
 	h.Write([]byte(record))
@@ -35,20 +34,155 @@ func calculateHash(block Block) string {
 	return hex.EncodeToString(hashed)
 }
 
-func generateBlock(oldBlock Block, BMP int) (Block, error) {
-	var newBlock Block
+func generateBlock(oldBlock Block, BPM int) (Block, error) {
+	var newBlock Block 
 
 	t := time.Now()
 
 	newBlock.Index = oldBlock.Index + 1
 	newBlock.Timestamp = t.String()
-	newBlock.BPM = BMP
-	newBlock.PrevHash = oldBlock.Hash
-	newBlock.Hash = calculateHash(oldBlock)
+	newBlock.BPM = BPM
+	newBlock.PrevHash = oldBlock.Hash 
+	newBlock.Hash = calulateHash(newBlock)
+
 	return newBlock, nil
 }
 
-// contunue with blockchain and block validation
+func isBlockValid(newBlock, oldBlock Block) bool {
+	if oldBlock.Index + 1 != newBlock.Index {
+		return false
+	}
+
+	if oldBlock.Hash != newBlock.PrevHash {
+		return false
+	}
+
+	if calulateHash(newBlock) != newBlock.Hash {
+		return false
+	}
+
+	return true
+}
+
+// always take the longest chain
+func replaceChain(newBlocks []Block) {
+	if len(newBlocks) > len(Blockchain) {
+		Blockchain = newBlocks
+	}
+}
+
+// create a web server
+func run() error {
+	mux := makeMuxRouter()
+	httpAddr := os.Getenv("ADDR")
+	log.Println("Listening on port: ", os.Getenv("ADDR"))
+	s := &http.Server{
+		Addr: 					":" + httpAddr,
+		Handler: 				mux,
+		ReadTimeout:  	10 * time.Second,
+		WriteTimeout: 	10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	if err := s.ListenAndServe(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func makeMuxRouter() http.Handler	{
+	muxRouter := mux.NewRouter()
+	muxRouter.HandleFunc("/", handleGetBlockchain).Methods("GET")
+	muxRouter.HandleFunc("/", handleWriteBlock).Methods("POST")
+	return muxRouter
+}
+
+func handleGetBlockchain(w http.ResponseWriter, r *http.Request) {
+	bytes, err := json.MarshalIndent(Blockchain, "", " ")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	io.WriteString(w, string(bytes))
+}
+
+type Message struct {
+	BPM int
+}
+
+func handleWriteBlock(w http.ResponseWriter, r *http.Request) {
+	var m Message
+
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&m); err != nil {
+		respondWithJSON(w, r, http.StatusBadRequest, r.Body)
+		return 
+	}
+
+	newBlock, err := generateBlock(Blockchain[len(Blockchain) - 1], m.BPM)
+	if err != nil {
+		respondWithJSON(w, r, http.StatusInternalServerError, m)
+		return
+	}
+	if isBlockValid(newBlock, Blockchain[len(Blockchain) - 1]) {
+		newBlockchain := append(Blockchain, newBlock)
+		replaceChain(newBlockchain)
+		spew.Dump(Blockchain)
+
+	}
+
+	respondWithJSON(w, r, http.StatusCreated, newBlock)
+}
+
+func respondWithJSON(w http.ResponseWriter, r *http.Request, code int, payload interface{}) {
+	response, err := json.MarshalIndent(payload, "", " ")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("HTTP 500: Internal Server Error"))
+		return
+	}
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go func() {
+		t := time.Now()
+		genesisBlock := Block{0, t.String(), 0, "", ""}
+		spew.Dump(genesisBlock)
+		Blockchain = append(Blockchain, genesisBlock)
+	}()
+	log.Fatal(run())
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
